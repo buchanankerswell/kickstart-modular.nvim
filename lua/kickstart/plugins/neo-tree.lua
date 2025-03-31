5772fc3a33934498ef5a89646e72f4a23de6c937
Buchanan Kerswell <buck.kerswell@gmail.com>
2025-03-31 13:42:56 +0100

    31 mar 2025 update; use normal text icons for neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -3,7 +3,6 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
@@ -11,7 +10,36 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    enable_git_status = true,
+    enable_diagnostics = true,
+    default_component_configs = {
+      icon = {
+        folder_closed = '[+]',
+        folder_open = '[-]',
+        folder_empty = '[ ]',
+        default = '*',
+      },
+      modified = {
+        symbol = '[M]',
+      },
+      git_status = {
+        symbols = {
+          added = '+',
+          modified = '~',
+          deleted = '-',
+          renamed = '»',
+          untracked = '?',
+          ignored = '!',
+          unstaged = '*',
+          staged = '✓',
+          conflict = '⚠',
+        },
+      },
+    },
     filesystem = {
+      follow_current_file = true,
+      hijack_netrw = true,
+      use_libuv_file_watcher = false,
       window = {
         mappings = {
           ['\\'] = 'close_window',
@@ -20,5 +48,3 @@ return {
     },
   },
 }
-
--- vim: ts=2 sts=2 sw=2 et
