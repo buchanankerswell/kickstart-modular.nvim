d64fa236247c0e41d6e21b9d6930ff3de2e273bb
Buchanan Kerswell <buck.kerswell@gmail.com>
2025-06-18 09:54:51 -0600

    18 jun 2025 update; add smart indent and expandtab

--- lua/options.lua
+++ lua/options.lua
@@ -19,4 +19,9 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
 -- vim: ts=2 sts=2 sw=2 et
